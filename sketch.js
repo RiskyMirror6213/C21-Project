385538df6b3916ad0f9f4b330f8c06969c97d59c
RiskyMirror6213 <53006341+RiskyMirror6213@users.noreply.github.com>
2020-05-23 18:00:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var bullet, wall, thickness;
+var speed, weight, deformation, damage;
+var ftx, fty;
+
+function setup() {
+  createCanvas(1600, 400);
+  speed = random(223, 321);
+  weight = random(30, 52);
+  thickness = random(22, 83)
+  bullet = createSprite(50, 200, 50, 10);
+  wall = createSprite(1200, 200, thickness, 200);
+  bullet.velocityX = speed;
+  wall.shapeColor = (80, 80, 80);
+  bullet.shapeColor = (255, 255, 255);
+  bullet.debug = false;
+  wall.debug = false;
+}
+
+function draw() {
+  background(0, 0, 0);
+  /*if(bullet.x - wall.x < wall.width/2 + bullet.width/2
+    && wall.x - bullet.x < wall.width/2 + bullet.width/2 && bullet.y - wall.y < wall.height/2 + bullet.height/2
+    && wall.y - bullet.y < wall.height/2 + bullet.height/2){
+    bullet.velocityX = 0;
+    deformation = (0.5*speed*speed*weight)/22500;
+  }
+  if(bullet.x - wall.x < wall.width/2 + bullet.width/2
+    && wall.x - bullet.x < wall.width/2 + bullet.width/2 && bullet.y - wall.y < wall.height/2 + bullet.height/2
+    && wall.y - bullet.y < wall.height/2 + bullet.height/2){
+    bullet.x = wall.x - thickness;
+  }
+  */
+ if(bullet.isTouching(wall)){
+   bullet.velocityX = 0;
+   bullet.x = wall.x - thickness;
+   damage = (0.5*weight*speed*speed)/(thickness*thickness*thickness)
+ }
+  if(damage > 10){
+    wall.shapeColor = "red"
+  }
+  if(damage <= 10){
+    wall.shapeColor = "green"
+  }
+  drawSprites();
+}
